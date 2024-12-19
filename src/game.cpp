8089b8b7428f5add2ce383c84ed82659f8c24f7d
Arek Sroka <erneth22@gmail.com>
2024-12-19 20:24:39 +0100

    merge

---
+++ src/game.cpp
@@ -0,0 +1,22 @@
+#include "game.h"
+
+void game::Run() {
+    auto window = sf::RenderWindow({640,380}, "Terraria At Home");
+
+    while (window.isOpen()) 
+    {
+        for (auto event = sf::Event(); window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed 
+            || (event.key.code == sf::Keyboard::Escape))
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(100,190,100,255));
+        
+        window.display();
+    }
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,6 @@
-#include <SFML/Graphics.hpp>
-
+#include "game.h"
 int main()
 {
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
-    window.setFramerateLimit(144);
-
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event(); window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
-    }
+    game game;
+    game.Run();
 }
